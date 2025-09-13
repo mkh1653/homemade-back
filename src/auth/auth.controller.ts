3b7bb89d0626d574d0e5ccbca95b8280788816a6
mkh1653 <m72.m27@gmail.com>
2025-09-13 21:15:27 +0330

    change cookie config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class AuthController {
       httpOnly: true,
       // secure: process.env.NODE_ENV === 'production',
       secure: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 3600000 * 24,
     });
 
